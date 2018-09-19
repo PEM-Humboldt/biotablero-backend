f4d5cc00ef08fed8630b06f6d105bbf0b10a87bb
Erika Suárez Valencia <marvinesv@gmail.com>
2018-09-19 13:55:30 -0500

    Transform geoJSON to topoJSON. Yet the response weights the same

--- src/service/biome.js
+++ src/service/biome.js
@@ -1,3 +1,13 @@
+const topojson = require('topojson');
+
 module.exports = biomePersistence => ({
-  getBiomeByEA: envAuthority => biomePersistence.findBiomeByEA(envAuthority),
+  getBiomeByEA: envAuthority => (
+    biomePersistence.findBiomeByEA(envAuthority)
+      .then(biomes => (
+        biomes.map(({ geomGeoJSON, ...others }) => ({
+          ...others,
+          geomTopoJSON: topojson.topology({ geomTopoJSON: JSON.parse(geomGeoJSON) }),
+        }))
+      ))
+  ),
 });

c33cdb7b87e3905a65fd7a2924a377982b6724e8
Erika Suárez Valencia <marvinesv@gmail.com>
2018-09-24 13:33:43 -0500

    Test apidoc generated doc

---
+++ docs/api_data.js
@@ -0,0 +1,12 @@
+define({ "api": [
+  {
+    "group": "testGroup",
+    "type": "get",
+    "url": "/hello/:name",
+    "title": "Test",
+    "version": "0.0.0",
+    "filename": "src/app.js",
+    "groupTitle": "testGroup",
+    "name": "GetHelloName"
+  }
+] });
--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const restify = require('restify');
 
 const server = restify.createServer();
 
+/**
+ * @apiGroup testGroup
+ * @api {get} /hello/:name Test
+ */
 server.get('/hello/:name', (req, res, next) => {
   res.send(`hello ${req.params.name}`);
   next();

171765156884fe0637ab3620d3976f175d2653b3
Erika Suárez Valencia <marvinesv@gmail.com>
2018-10-09 16:04:56 -0500

    [WIP]

--- src/persistence/biome.js
+++ src/persistence/biome.js
@@ -16,11 +16,12 @@ module.exports = (bookshelfConn) => {
         error.code = 400;
         throw error;
       }
+      // 2.66
       return knex.raw(
         `SELECT jsonb_build_object(
           'type', 'Feature',
           'gid', gid,
-          'geometry', ST_AsGeoJSON(ST_Simplify(geom, 0.0015, true))::jsonb,
+          'geometry', ST_AsGeoJSON(ST_SimplifyPreserveTopology(geom, 0.001))::jsonb,
           'properties', to_jsonb(row) - 'gid' - 'geom'
         ) as json_object
         FROM (SELECT * FROM geo_ea_biomes) row
@@ -31,6 +32,23 @@ module.exports = (bookshelfConn) => {
           totalFeatures: biomes.rowCount,
           features: biomes.rows.map(f => f.json_object),
         }));
+
+      // 2.9
+      // return knex.raw(
+      //   `SELECT row_to_json(fc)
+      //   FROM (
+      //     SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features
+      //     FROM(
+      //       SELECT 'Feature' as type,
+      //         row_to_json(geo2) As properties,
+      //         st_asgeojson(ST_SimplifyPreserveTopology(geom,0.001))::json as geometry
+      //       FROM geo_ea_biomes as geo1
+      //       inner join(select gid,name_biome  from geo_ea_biomes) as geo2 on geo2.gid=geo1.gid
+      //       WHERE geo1.id_ea='${envAuthority}'
+      //     ) as f
+      //   ) as fc`,
+      // )
+      //   .then(biomes => biomes.rows[0].row_to_json);
     },
   };
 };

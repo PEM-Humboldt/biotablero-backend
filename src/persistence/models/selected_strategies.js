4cbf4030a7726e8b3dc4a54ff34fdc9f1845e604
Erika Suárez Valencia <marvinesv@gmail.com>
2018-10-09 12:02:32 -0500

    Organize event handler function to be reused

--- src/persistence/models/selected_strategies.js
+++ src/persistence/models/selected_strategies.js
@@ -2,27 +2,19 @@ const requiredFields = ['id_biome', 'id_ea', 'id_h_subzone', 'id_strategy', 'are
   'id_user'];
 
 /**
- * Event: http://bookshelfjs.org/index.html#Model-event-saving
- * Note: There's currently a bug that leads to attrs only containing attributes that were
- * passed as argument to save. You can work around this by accessing model.changed which
- * does contain all the attributes that will be inserted or updated.
+ * Create a model for the selected_strategies table
+ *
+ * @param {Object} bookshelf bookshelf ref to create the model
+ * @param {Object} param1 default event handlers, see util/events to see available ones
  */
-const onSaving = (model) => {
-  const missing = requiredFields.filter(field => !model.changed[field]);
-  if (missing.length > 0) {
-    const error = new Error(`The following properties are missing a value: ${missing}`);
-    error.code = 400;
-    throw error;
-  }
-};
-
-module.exports = bookshelf => (
+module.exports = (bookshelf, { saving }) => (
   bookshelf.Model.extend({
     tableName: 'selected_strategies',
     defaults: { register_date: new Date() },
     constructor: function constructor(...args) {
       bookshelf.Model.apply(this, args);
-      this.on('saving', onSaving);
+      // See note on http://bookshelfjs.org/index.html#Model-event-saving
+      this.on('saving', model => saving(requiredFields, model.changed));
     },
   })
 );

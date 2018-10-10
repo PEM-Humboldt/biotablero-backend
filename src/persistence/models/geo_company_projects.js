f7eca500fa5a52a498c58f95c5da195ef161a617
Erika Suárez Valencia <marvinesv@gmail.com>
2018-10-10 14:33:46 -0500

    Improve param name in model

--- src/persistence/models/geo_company_projects.js
+++ src/persistence/models/geo_company_projects.js
@@ -4,7 +4,7 @@ const requiredFields = ['name', 'id_company', 'id_region'];
  * Create a model for the geo_company_projects table
  *
  * @param {Object} bookshelf bookshelf ref to create the model
- * @param {Object} param1 default event handlers, see util/events to see available ones
+ * @param {Object} handlers default event handlers, see util/events to see available ones
  */
 module.exports = (bookshelf, { saving }) => (
   bookshelf.Model.extend({

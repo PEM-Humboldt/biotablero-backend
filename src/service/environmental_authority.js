92a06fc9f8ef8f029fa9aac3545e49f16e2697af
Erika Suárez Valencia <marvinesv@gmail.com>
2018-12-04 15:15:16 -0500

    Change group logic for area by cf

--- src/service/environmental_authority.js
+++ src/service/environmental_authority.js
@@ -8,17 +8,22 @@ module.exports = eaPersistence => ({
    */
   getAreaByCF: async (envAuthorityId) => {
     const areas = await eaPersistence.findAreaByCF(envAuthorityId);
-    const groupedAreas = {};
+    const keys = [];
+    const values = [];
     areas.forEach((area) => {
-      const fc = parseFloat(area.fc_valor);
+      let fc = parseFloat(area.fc_valor);
       const fcDiff = fc - parseInt(fc, 10);
-      let key = fc;
-      if (fcDiff !== 0 && fcDiff !== 0.5) key = fc + 0.25;
-      if (!groupedAreas[key]) groupedAreas[key] = 0;
-      groupedAreas[key] += parseFloat(area.sum);
+      if (fcDiff !== 0 && fcDiff !== 0.5) fc += 0.25;
+      const key = keys.findIndex(e => e === fc);
+      if (key === -1) {
+        keys.push(fc);
+        values.push(area.sum);
+      } else {
+        values[key] += area.sum;
+      }
     });
 
-    return Object.keys(groupedAreas).sort().map(fc => ({ key: fc, area: groupedAreas[fc] }));
+    return keys.map((fc, idx) => ({ key: fc, area: values[idx] }));
   },
 
   /**

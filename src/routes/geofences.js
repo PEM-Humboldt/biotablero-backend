6b74b3d97cf71ad96490bf3d4bcf5a2628e7d9bf
Erika Suárez Valencia <marvinesv@gmail.com>
2019-02-25 10:35:18 -0500

    Create routes for geofences endpoints

--- src/routes/geofences.js
+++ src/routes/geofences.js
@@ -20,6 +20,7 @@ const { Router } = require('restify-router');
  *    }...
  *  ]
  */
+// TODO: Inject other services
 module.exports = (errorHandler, eaService) => {
   const router = new Router();
 
@@ -46,6 +47,108 @@ module.exports = (errorHandler, eaService) => {
         next();
       })
   )));
+
+  /**
+   * @apiGroup geofences
+   * @api {get} /geofences/departments listDepartments
+   * @apiName listDepartments
+   * @apiVersion 0.1.0
+   * @apiDescription
+   * List all available departments
+   *
+   * @apiSuccess {Object[]} departments list of departments
+   * @apiSuccess TODO list other response attributes
+   *
+   * @apiExample {curl} Example usage:
+   *  /geofences/departments
+   * TODO: Add response example
+   */
+  router.get('/geofences/departments', errorHandler());
+
+  /**
+   * @apiGroup geofences
+   * @api {get} /geofences/municipalities listMunicipalities
+   * @apiName listMunicipalities
+   * @apiVersion 0.1.0
+   * @apiDescription
+   * List all available municipalities
+   *
+   * @apiSuccess {Object[]} municipalities list of municipalities
+   * @apiSuccess TODO list other response attributes
+   *
+   * @apiExample {curl} Example usage:
+   *  /geofences/municipalities
+   * TODO: Add response example
+   */
+  router.get('/geofences/municipalities', errorHandler());
+
+  /**
+   * @apiGroup geofences
+   * @api {get} /geofences/basins listBasins
+   * @apiName listBasins
+   * @apiVersion 0.1.0
+   * @apiDescription
+   * List all available basins
+   *
+   * @apiSuccess {Object[]} basins list of basins
+   * @apiSuccess TODO list other response attributes
+   *
+   * @apiExample {curl} Example usage:
+   *  /geofences/basins
+   * TODO: Add response example
+   */
+  router.get('/geofences/basins', errorHandler());
+
+  /**
+   * @apiGroup geofences
+   * @api {get} /geofences/sub-basins listSub-basins
+   * @apiName listSub-basins
+   * @apiVersion 0.1.0
+   * @apiDescription
+   * List all available sub-basins
+   *
+   * @apiSuccess {Object[]} sub-basins list of sub-basins
+   * @apiSuccess TODO list other response attributes
+   *
+   * @apiExample {curl} Example usage:
+   *  /geofences/sub-basins
+   * TODO: Add response example
+   */
+  router.get('/geofences/sub-basins', errorHandler());
+
+  /**
+   * @apiGroup geofences
+   * @api {get} /geofences/protectedAreas listProtectedAreas
+   * @apiName listProtectedAreas
+   * @apiVersion 0.1.0
+   * @apiDescription
+   * List all available protected areas
+   *
+   * @apiSuccess {Object[]} protectedAreas list of protected areas
+   * @apiSuccess TODO list other response attributes
+   *
+   * @apiExample {curl} Example usage:
+   *  /geofences/protectedAreas
+   * TODO: Add response example
+   */
+  router.get('/geofences/protectedAreas', errorHandler());
+
+  /**
+   * @apiGroup geofences
+   * @api {get} /geofences/strategicEcosystems listStrategicEcosystems
+   * @apiName listStrategicEcosystems
+   * @apiVersion 0.1.0
+   * @apiDescription
+   * List all available strategic ecosystems
+   *
+   * @apiSuccess {Object[]} strategicEcosystems list of strategic ecosystems
+   * @apiSuccess TODO list other response attributes
+   *
+   * @apiExample {curl} Example usage:
+   *  /geofences/strategicEcosystems
+   * TODO: Add response example
+   */
+  router.get('/geofences/strategicEcosystems', errorHandler());
 
   return router;
 };

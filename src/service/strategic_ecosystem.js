302144efd6ace9e915ac13f6ef3778986745f3c6
Erika Suárez Valencia <marvinesv@gmail.com>
2019-04-02 15:39:04 -0500

    Separate service functions to match new struture in the mockups

--- src/service/strategic_ecosystem.js
+++ src/service/strategic_ecosystem.js
@@ -116,68 +116,95 @@ module.exports = (
    *
    * @param {String} ecosystem ecosystem type to get information
    */
-  getAreasByEcosystem: async (ecosystem) => {
+  getEcosystemNatInfo: async (ecosystem) => {
     const countryArea = await paramoPersistence.findCountryTotalArea();
-    let national;
-    let coverageAreas;
-    let protectedAreas;
+    let national = {};
     switch (ecosystem) {
       case 'Páramo': {
         national = await paramoPersistence.findTotalArea();
-        coverageAreas = await paramoPersistence.findCoverAreas();
-        protectedAreas = await paramoPersistence.findProtectedAreas();
         break;
       }
       case 'Humedal': {
         national = await wetlandPersistence.findTotalArea();
-        coverageAreas = await wetlandPersistence.findCoverAreas();
-        protectedAreas = await wetlandPersistence.findProtectedAreas();
         break;
       }
       case 'Bosque Seco Tropical': {
         national = await tropicalDryForestPersistence.findTotalArea();
-        coverageAreas = await tropicalDryForestPersistence.findCoverAreas();
-        protectedAreas = await tropicalDryForestPersistence.findProtectedAreas();
         break;
       }
       default:
         return {};
     }
     return {
-      national: {
-        ...national[0],
-        percentage: national[0].area / countryArea[0].area,
-        type: ecosystem,
-      },
-      coverage: coverageAreas.map(area => ({
-        ...area,
-        percentage: area.area / national[0].area,
-      })),
-      pa: protectedAreas.map(area => ({
-        ...area,
-        percentage: area.area / national[0].area,
-      })),
+      ...national[0],
+      percentage: national[0].area / countryArea[0].area,
+      type: ecosystem,
     };
   },
-  getEcosystemNatInfo: async ecosystem => ({ area: 123456789, percentage: 0.45, type: ecosystem }),
 
   /**
    * Get coverage information for the given ecosystem
    *
    * @param {String} ecosystem ecosystem type to get information
    */
-  getSEByCoverage: async ecosystem => ([
-    { percentage: 0.25, type: 'narutal' },
-    { percentage: 0.1, type: 'transformed' },
-  ]),
+  getSEByCoverage: async (ecosystem) => {
+    let national = {};
+    let coverageAreas = [];
+    switch (ecosystem) {
+      case 'Páramo': {
+        national = await paramoPersistence.findTotalArea();
+        coverageAreas = await paramoPersistence.findCoverAreas();
+        break;
+      }
+      case 'Humedal': {
+        national = await wetlandPersistence.findTotalArea();
+        coverageAreas = await wetlandPersistence.findCoverAreas();
+        break;
+      }
+      case 'Bosque Seco Tropical': {
+        national = await tropicalDryForestPersistence.findTotalArea();
+        coverageAreas = await tropicalDryForestPersistence.findCoverAreas();
+        break;
+      }
+      default:
+        return {};
+    }
+    return coverageAreas.map(area => ({
+      ...area,
+      percentage: area.area / national[0].area,
+    }));
+  },
 
   /**
    * Get protected areas information for the given ecosystem
    *
    * @param {String} ecosystem ecosystem type to get information
    */
-  getSEByPA: async ecosystem => ([
-    { percentage: 0.04, category: 'Reserva Natural de la Sociedad Civil' },
-    { percentage: 0.1, category: 'Parque Nacional Natural' },
-  ]),
+  getSEByPA: async (ecosystem) => {
+    let national;
+    let protectedAreas;
+    switch (ecosystem) {
+      case 'Páramo': {
+        national = await paramoPersistence.findTotalArea();
+        protectedAreas = await paramoPersistence.findProtectedAreas();
+        break;
+      }
+      case 'Humedal': {
+        national = await wetlandPersistence.findTotalArea();
+        protectedAreas = await wetlandPersistence.findProtectedAreas();
+        break;
+      }
+      case 'Bosque Seco Tropical': {
+        national = await tropicalDryForestPersistence.findTotalArea();
+        protectedAreas = await tropicalDryForestPersistence.findProtectedAreas();
+        break;
+      }
+      default:
+        return {};
+    }
+    return protectedAreas.map(area => ({
+      ...area,
+      percentage: area.area / national[0].area,
+    }));
+  },
 });

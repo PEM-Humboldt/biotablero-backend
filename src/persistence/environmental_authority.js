d8a0dde5f6108df29e8dde139a9f438325bbeda5
Erika Suárez Valencia <marvinesv@gmail.com>
2019-04-22 10:03:10 -0500

    Implement national layer for environmental authorities

--- src/persistence/environmental_authority.js
+++ src/persistence/environmental_authority.js
@@ -1,88 +1,117 @@
+const config = require('config');
+
 module.exports = (
   db,
   { colombiaDetails, eaBioticUnits, geoEnvironmentalAuthorities },
-) => ({
-  /**
-   * Find total area grouped by compensation factor in a given environmental authority
-   *
-   * @param {String} envAuthorityId environmental authority id
-   *
-   * @returns {Object[]} total areas by compensation factor
-   */
-  findAreaByCF: envAuthorityId => (
-    colombiaDetails.query()
-      .where('idcar', envAuthorityId)
-      .sum('area_ha')
-      .groupBy('fc_valor')
-      .orderBy('fc_valor', 'asc')
-      .select('fc_valor')
-  ),
+) => {
+  const geometriesConfig = config.geometries;
 
-  /**
-   * Find total area grouped by biotic unit in a given environmental authority
-   *
-   * @param {String} envAuthorityId environmental authority id
-   *
-   * @returns {Object[]} total areas by biotic unit
-   */
-  findAreaByBioticUnit: envAuthorityId => (
-    eaBioticUnits.query()
-      .where('id_ea', envAuthorityId)
-      .sum('area_ha as area')
-      .groupBy('name')
-      .orderBy('name', 'asc')
-      .select('name as key')
-  ),
+  return {
+    /**
+     * Find total area grouped by compensation factor in a given environmental authority
+     *
+     * @param {String} envAuthorityId environmental authority id
+     *
+     * @returns {Object[]} total areas by compensation factor
+     */
+    findAreaByCF: envAuthorityId => (
+      colombiaDetails.query()
+        .where('idcar', envAuthorityId)
+        .sum('area_ha')
+        .groupBy('fc_valor')
+        .orderBy('fc_valor', 'asc')
+        .select('fc_valor')
+    ),
 
-  /**
-   * Find total area grouped by biome in a given environmental authority
-   *
-   * @param {String} envAuthorityId environmental authority id
-   *
-   * @returns {Object[]} total areas by biome
-   */
-  findAreaByBiome: envAuthorityId => (
-    colombiaDetails.query()
-      .where('idcar', envAuthorityId)
-      .sum('area_ha as area')
-      .groupBy('bioma_prel')
-      .orderBy('bioma_prel', 'asc')
-      .select('bioma_prel as key')
-  ),
+    /**
+     * Find total area grouped by biotic unit in a given environmental authority
+     *
+     * @param {String} envAuthorityId environmental authority id
+     *
+     * @returns {Object[]} total areas by biotic unit
+     */
+    findAreaByBioticUnit: envAuthorityId => (
+      eaBioticUnits.query()
+        .where('id_ea', envAuthorityId)
+        .sum('area_ha as area')
+        .groupBy('name')
+        .orderBy('name', 'asc')
+        .select('name as key')
+    ),
 
-  /**
-   * Find total area grouped by sub-basin in a given environmental authority filtered by biome
-   *
-   * @param {String} envAuthorityId environmental authority id
-   * @param {String} biomeName biome name
-   *
-   * @returns {Object[]} total areas by sub-basin
-   */
-  findBiomeAreaBySubzone: (envAuthorityId, biomeName) => (
-    colombiaDetails.query()
-      .where({ idcar: envAuthorityId, bioma_iavh: biomeName })
-      .sum('area_ha as area')
-      .groupBy('nomszh')
-      .orderBy('nomszh', 'asc')
-      .select('nomszh as key')
-  ),
+    /**
+     * Find total area grouped by biome in a given environmental authority
+     *
+     * @param {String} envAuthorityId environmental authority id
+     *
+     * @returns {Object[]} total areas by biome
+     */
+    findAreaByBiome: envAuthorityId => (
+      colombiaDetails.query()
+        .where('idcar', envAuthorityId)
+        .sum('area_ha as area')
+        .groupBy('bioma_prel')
+        .orderBy('bioma_prel', 'asc')
+        .select('bioma_prel as key')
+    ),
 
-  /**
-   * Get all environmental authorities id and name
-   */
-  findAll: () => (
-    geoEnvironmentalAuthorities.query()
-      .select('id_ea as id', 'name')
-  ),
+    /**
+     * Find total area grouped by sub-basin in a given environmental authority filtered by biome
+     *
+     * @param {String} envAuthorityId environmental authority id
+     * @param {String} biomeName biome name
+     *
+     * @returns {Object[]} total areas by sub-basin
+     */
+    findBiomeAreaBySubzone: (envAuthorityId, biomeName) => (
+      colombiaDetails.query()
+        .where({ idcar: envAuthorityId, bioma_iavh: biomeName })
+        .sum('area_ha as area')
+        .groupBy('nomszh')
+        .orderBy('nomszh', 'asc')
+        .select('nomszh as key')
+    ),
 
-  /**
-   * Get the total area for the given environmental authority
-   *
-   * @param {String} envAuthorityId EA id
-   */
-  getTotalAreaByEA: envAuthorityId => (
-    geoEnvironmentalAuthorities.query()
-      .where({ id_ea: envAuthorityId })
-      .select('area_ha as area')
-  ),
-});
+    /**
+     * Get all environmental authorities id and name
+     */
+    findAll: () => (
+      geoEnvironmentalAuthorities.query()
+        .select('id_ea as id', 'name')
+    ),
+
+    /**
+     * Get the total area for the given environmental authority
+     *
+     * @param {String} envAuthorityId EA id
+     */
+    getTotalAreaByEA: envAuthorityId => (
+      geoEnvironmentalAuthorities.query()
+        .where({ id_ea: envAuthorityId })
+        .select('area_ha as area')
+    ),
+
+    /**
+     * Get GeoJson layer with environmental authorities at national level
+     */
+    findNationalLayer: () => (
+      db.raw(
+        `SELECT row_to_json(fc) as collection
+        FROM (
+          SELECT 'FeatureCollection' as type, array_to_json(array_agg(f)) as features
+          FROM(
+            SELECT 'Feature' as type,
+              row_to_json(ea2) as properties,
+              ST_AsGeoJSON(ST_SimplifyPreserveTopology(geom, ${geometriesConfig.tolerance}))::json as geometry
+            FROM geo_environmental_authorities as ea1
+            INNER JOIN (
+              SELECT gid, id_ea, name, area_ha
+              FROM geo_environmental_authorities
+            ) as ea2 ON ea1.gid = ea2.gid
+          ) as f
+        ) as fc`,
+      )
+        .then(layers => layers.rows[0].collection)
+    ),
+  };
+};

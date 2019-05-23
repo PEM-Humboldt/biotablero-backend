20d2f2fb4d021b6c2f4da09dd828153601e92cdf
Erika Suárez Valencia <marvinesv@gmail.com>
2019-05-23 09:28:04 -0500

    Organize function in tropical dry forest persistence

--- src/persistence/tropicalDryForest.js
+++ src/persistence/tropicalDryForest.js
@@ -87,19 +87,6 @@ module.exports = (db, { geoTropicalDryForestDetails }) => ({
   ),
 
   /**
-   * Find areas grouped by protected area category
-   *
-   * @param {Number} year optional year to filter data, 2012 by default
-   */
-  findProtectedAreas: async (year = 2012) => (
-    db('geo_tropical_dry_forest_details')
-      .innerJoin('geo_protected_areas', 'geo_tropical_dry_forest_details.id_protected_area', 'geo_protected_areas.gid')
-      .where({ 'geo_tropical_dry_forest_details.year_cover': year })
-      .groupBy('geo_protected_areas.category')
-      .select(db.raw('coalesce(SUM(geo_tropical_dry_forest_details.area_ha), 0) as area'), 'geo_protected_areas.category as type')
-  ),
-
-  /**
    * Find areas grouped by cover type inside the given protected area category
    *
    * @param {String} categoryName protected area category
@@ -111,5 +98,18 @@ module.exports = (db, { geoTropicalDryForestDetails }) => ({
       .where({ 'geo_protected_areas.category': categoryName, 'geo_tropical_dry_forest_details.year_cover': year })
       .groupBy('geo_tropical_dry_forest_details.area_type')
       .select(db.raw('coalesce(SUM(geo_tropical_dry_forest_details.area_ha), 0) as area'), 'geo_tropical_dry_forest_details.area_type as type')
+  ),
+
+  /**
+   * Find areas grouped by protected area category
+   *
+   * @param {Number} year optional year to filter data, 2012 by default
+   */
+  findProtectedAreas: async (year = 2012) => (
+    db('geo_tropical_dry_forest_details')
+      .innerJoin('geo_protected_areas', 'geo_tropical_dry_forest_details.id_protected_area', 'geo_protected_areas.gid')
+      .where({ 'geo_tropical_dry_forest_details.year_cover': year })
+      .groupBy('geo_protected_areas.category')
+      .select(db.raw('coalesce(SUM(geo_tropical_dry_forest_details.area_ha), 0) as area'), 'geo_protected_areas.category as type')
   ),
 });

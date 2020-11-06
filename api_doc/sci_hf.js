7dd5fda177e1d89be15d270732673e429577e539
Erika Suárez Valencia <esuarez@humboldt.org.co>
2020-11-06 14:05:30 -0500

    Mockup for sci_hf in basin subzones

---
+++ api_doc/sci_hf.js
@@ -0,0 +1,25 @@
+/**
+ * @apiDefine sci_hf Search > Forest Structural Condition Index (with Human Footprint)
+ * Information related to the forest structural condition index crossed with human footprint in a
+ * given geofence.
+ */
+
+/**
+ * @apiDefine SCIHFPAxample
+ * @apiSuccessExample {json} Success-Example:
+ * [
+ *   {
+ *     "hf_pers": "estable_alta",
+ *     "sci_cat": "alta",
+ *     "pa": "PNN",
+ *     "area": 101834
+ *   },
+ *   {
+ *     "hf_pers": "estable_alta",
+ *     "sci_cat": "alta",
+ *     "pa": "PNR",
+ *     "area": 134886
+ *   },
+ *   ...
+ * ]
+ */

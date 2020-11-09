92426bdce98c68fcde65c646e468fd4e6f40cb36
Camilo <cazm013@gmailcom>
2020-11-09 15:27:33 -0500

    New api doc group to ecoChange information

---
+++ api_doc/ecoChange.js
@@ -0,0 +1,37 @@
+/**
+ * @apiDefine s_ecoChange Search > Forest
+ * Information related to forest  in a given geofence.
+ */
+
+/**
+ * @apiDefine ForestLPAxample
+ * @apiSuccessExample {json} Success-Example:
+ * [
+ *   {
+ *   "id": "2016-2019",
+ *   "data": [
+ *     {
+ *       "area": 50000,
+ *       "key": "persistencia",
+ *       "percentage": 0.50
+ *     },
+ *      {
+ *       "area": 15000,
+ *       "key": "perdida",
+ *       "percentage": 0.15
+ *     },
+ *     {
+ *       "area": 25000,
+ *       "key": "ganancia",
+ *       "percentage": 0.25
+ *     },
+ *     {
+ *       "area": 10000,
+ *       "key": "no_bosque",
+ *       "percentage": 0.10
+ *     }
+ *   ]
+ *  },
+ *   ...
+ * ]
+ */

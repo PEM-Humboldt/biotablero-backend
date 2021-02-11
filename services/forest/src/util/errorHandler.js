e560367595072c438df870227e928839964f802d
Camilo Zapata <cazm013@gmail.com>
2021-02-11 17:25:26 -0500

    Add info logs

--- services/forest/src/util/errorHandler.js
+++ services/forest/src/util/errorHandler.js
@@ -8,8 +8,10 @@
  */
 module.exports = logger => callback => (
   async (req, res, next) => {
+    logger.info(`[${req.id()}] received - ${req.href()}`);
     try {
       await callback(req, res, next);
+      logger.info(`[${req.id()}] finished - ${req.href()}`);
     } catch (e) {
       const code = typeof e.code === 'number' ? e.code : 500;
       logger.error(e.stack || e.Error || e.message || e);

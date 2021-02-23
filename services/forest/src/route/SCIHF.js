3c77d975c30886703ab1e01cce8d78dbe41393f7
Camilo Zapata <cazm013@gmail.com>
2021-02-23 18:58:32 -0500

    SCIHF routes

--- services/forest/src/route/SCIHF.js
+++ services/forest/src/route/SCIHF.js
@@ -4,30 +4,99 @@ module.exports = (errorHandler, SCIHFService) => {
   const router = new Router();
 
   /**
-   * @apiGroup sci_hf
+   * @apiGroup s_sci_hf
    * @api {get} /sci/hf/:areaType/:areaId SCIHF
    * @apiName SCIHF
    * @apiVersion 1.0.0
    * @apiDescription
-   * Forest Structural Condition Index for a given area
+   * Values for the forest structural condition index crossed with human footprint
+   * and protected area categories for a given area
    *
    * Value calculated for 2018
    *
-   * @apiParam (Path params) {String | Number} areaType area type
-   * @apiParam (Path params) {String} areaId area id
+   * @apiParam (Query params) {String | Number} areaType area type
+   * @apiParam (Query params) {String} areaId area id
    *
-   * @apiSuccess {Object} result
+   * @apiSuccess {Object[]} result
    * @apiSuccess {String} result.hf_pers Persistence category
    * @apiSuccess {String} result.sci_cat SCI categoy
    * @apiSuccess {String} result.pa Protected area category name
    * @apiSuccess {String} result.area Area inside the given area
    *
    * @apiExample {curl} Example usage:
-   *  /sci/hf/ea/DAGMA
+   *  /sci/hf?areaType=ea&areaId=DAGMA
    * @apiUse SCIHFExample
    */
-  router.get('/sci/hf/:areaType/:areaId', errorHandler((req, res, next) => (
+  router.get('/sci/hf', errorHandler((req, res, next) => (
     SCIHFService.getSCIHF(req.params.areaType, req.params.areaId)
+      .then((value) => {
+        res.send(value);
+        next();
+      })
+  )));
+
+  /**
+   * @apiGroup s_sci_hf
+   * @api {get} /sci/hf/layer/:areaType/:areaId SCIHFLayer
+   * @apiName SCIHFLayer
+   * @apiVersion 1.0.0
+   * @apiDescription
+   * Layer of the forest structural condition index crossed with human footprint for a given area
+   *
+   * Value calculated for 2018
+   *
+   * @apiParam (Query params) {String | Number} areaType area type
+   * @apiParam (Query params) {String} areaId area id
+   *
+   * @apiSuccess (geojson) {Object[]} result
+   * @apiSuccess (geojson) {String} result.type The geometry type
+   * @apiSuccess (geojson) {Object[]} result.features features information
+   * (type, properties, geometry)
+   *
+   * @apiExample {curl} Example usage:
+   *  /sci/hf/layer?areaType=ea&areaId=DAGMA
+   * @apiUse SCIHFLayerExample
+   */
+  router.get('/sci/hf/layer', errorHandler((req, res, next) => (
+    SCIHFService.getSCIHFLayer(req.params.areaType, req.params.areaId)
+      .then((value) => {
+        res.send(value);
+        next();
+      })
+  )));
+
+  /**
+   * @apiGroup s_sci_hf
+   * @api {get} /sci/:sci_cat/hf/:hf_pers/layer/:areaType/:areaId SCIHFPALayer
+   * @apiName SCIHFPALayer
+   * @apiVersion 1.0.0
+   * @apiDescription
+   * Layer of one combination of forest structural condition index category and a human
+   * footprint persistence category, divided by protected areas for a given area
+   *
+   * Value calculated for 2018
+   *
+   * @apiParam (Path params) {String} sciCat sci category
+   * @apiParam (Path params) {String} hfPers human footprint persistence category
+   * @apiParam (Query params) {String | Number} areaType area type
+   * @apiParam (Query params) {String} areaId area id
+   *
+   * @apiSuccess (geojson) {Object[]} result
+   * @apiSuccess (geojson) {String} result.type The geometry type
+   * @apiSuccess (geojson) {Object[]} result.features features information
+   * (type, properties, geometry)
+   *
+   * @apiExample {curl} Example usage:
+   *  /sci/baja_moderada/hf/estable_alta/layer?areaType=ea&areaId=DAGMA
+   * @apiUse SCIHFPALayerExample
+   */
+  router.get('/sci/:sciCat/hf/:hfPers/layer', errorHandler((req, res, next) => (
+    SCIHFService.getSCIHFPALayer(
+      req.params.sciCat,
+      req.params.hfPers,
+      req.params.areaType,
+      req.params.areaId,
+    )
       .then((value) => {
         res.send(value);
         next();

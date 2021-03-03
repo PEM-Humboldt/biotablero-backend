7b4c93af00f8892fc3df48125cf6b93f0f5a2d42
Erika Suárez Valencia <esuarez@humboldt.org.co>
2021-03-03 10:38:06 -0500

    remove integrity endpoints for states

--- services/backend/src/routes/states.js
+++ services/backend/src/routes/states.js
@@ -386,37 +386,6 @@ module.exports = (errorHandler, stateService) => {
   )));
 
   /**
-   * @apiGroup sci
-   * @api {get} /states/:state_id/sci/hf SCIHFInState
-   * @apiName SCIHFInState
-   * @apiVersion 0.1.0
-   * @apiDescription
-   * Values for the forest structural condition index crossed with human footprint and protected
-   * area categories inside the given state
-   *
-   * Values based on human footprint for 2018
-   *
-   * @apiParam (Path params) {Number} state_id state id
-   *
-   * @apiSuccess {Object[]} result list of objects with information about sci, hf and pa
-   * @apiSuccess {String} result.hf_pers human footprint persistence category
-   * @apiSuccess {String} result.sci_cat forest structural condition index category
-   * @apiSuccess {String} result.pa protected area category
-   * @apiSuccess {Number} result.area Area inside the state for hf_pers, sci_cat and pa
-   *
-   * @apiExample {curl} Example usage:
-   *  /states/86/sci/hf
-   * @apiUse SCIHFPAxample
-   */
-  router.get('/states/:state_id/sci/hf', errorHandler((req, res, next) => (
-    stateService.getSCIHF(req.params.state_id)
-      .then((values) => {
-        res.send(values);
-        next();
-      })
-  )));
-
-  /**
    * @apiGroup s_ecoChange
    * @api {get} /states/:state_id/ecoChange/lp/categories ForestLPInState
    * @apiName ForestLPInState
@@ -628,64 +597,6 @@ module.exports = (errorHandler, stateService) => {
     stateService.getHFPersistenceLayerById(req.params.state_id)
       .then((geometry) => {
         res.send(geometry);
-        next();
-      })
-  )));
-
-  /**
-   * @apiGroup sci
-   * @api {get} /states/:state_id/sci/hf/layers SCIHFLayerInState
-   * @apiName SCIHFLayerInState
-   * @apiVersion 0.1.0
-   * @apiDescription
-   * Get the sci - hf persistence layer in a given state
-   *
-   * @apiParam (Path params) {Number} state_id state id
-   *
-   * @apiSuccess (geojson) {Object[]} result
-   * @apiSuccess (geojson) {String} result.type The geometry type
-   * @apiSuccess (geojson) {Object[]} result.features features information
-   * (type, properties, geometry)
-   *
-   * @apiExample {curl} Example usage:
-   *  /states/44/sci/hf/layers
-   * @apiUse SCIHFInGeofenceExample
-   */
-  router.get('/states/:state_id/sci/hf/layers', errorHandler((req, res, next) => (
-    stateService.getSCIHFLayerById(req.params.state_id)
-      .then((values) => {
-        res.send(values);
-        next();
-      })
-  )));
-
-  /**
-   * @apiGroup sci
-   * @api {get} /states/:state_id/sci/:sci_cat/hf/:hf_pers/layers/pa SCIHFPALayerInState
-   * @apiName SCIHFPALayerInState
-   * @apiVersion 0.1.0
-   * @apiDescription
-   * Get the sci - hf persistence layer divided by protected areas in a given state
-   *
-   * @apiParam (Path params) {Number} state_id state id
-   * @apiParam (Path params) {String} sci_cat structural condition index category
-   * @apiParam (Path params) {String} hf_pers human footprint persistence category
-   *
-   * @apiSuccess (geojson) {Object[]} result
-   * @apiSuccess (geojson) {String} result.type The geometry type
-   * @apiSuccess (geojson) {Object[]} result.features features information
-   * (type, properties, geometry)
-   *
-   * @apiExample {curl} Example usage:
-   *  /states/44/sci/moderada/hf/dinamica/layers/pa
-   * @apiUse SCIHFPAInGeofenceExample
-   */
-  router.get('/states/:state_id/sci/:sci_cat/hf/:hf_pers/layers/pa', errorHandler((req, res, next) => (
-    stateService.getSCIHFPALayer(
-      req.params.state_id, req.params.sci_cat, req.params.hf_pers,
-    )
-      .then((values) => {
-        res.send(values);
         next();
       })
   )));

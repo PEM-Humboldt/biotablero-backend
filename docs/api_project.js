a8383d8a2985651776fb2bfc2a9bd5ee4178bb72
Erika Suárez Valencia <esuarez@humboldt.org.co>
2021-03-09 10:45:31 -0500

    Update api docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,14 +1,14 @@
 define({
   "name": "Biotablero REST API",
-  "version": "0.1.0",
+  "version": "1.3.0",
   "description": "Backend services for Biotablero",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-02-11T14:10:55.417Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.6"
+    "time": "2021-03-09T15:44:54.450Z",
+    "url": "https://apidocjs.com",
+    "version": "0.26.0"
   }
 });

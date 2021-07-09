9c3045431eaabe3a5ec7e2671c32f6809b338ed8
Trjegul84 <angelachek@gmail.com>
2021-07-08 20:05:52 -0500

    change name to match only richnes_nos querys

--- services/diversity/src/persistence/richness_nos.js
+++ services/diversity/src/persistence/richness_nos.js
@@ -1,4 +1,13 @@
-module.exports = db => ({
+const { areaTypeKeys } = require('../util/appropriate_keys');
+
+module.exports = (
+  db,
+  {
+    richnessNos,
+    richnessNosRegions,
+  },
+  logger,
+) => ({
   /**
    * Find the layer for the number of species in the given area of the given group
    *
@@ -33,7 +42,10 @@ module.exports = db => ({
       [filename, geometry, geometry],
     )
       .then(rast => rast.rows[0].image)
-      .catch()
+      .catch((e) => {
+        logger.error(e.stack || e.Error || e.message || e);
+        throw new Error('Error getting data');
+      })
   ),
 
   /**
@@ -72,4 +84,45 @@ module.exports = db => ({
       .then(rast => rast.rows[0])
       .catch()
   ),
+
+  /**
+     * Get values for the number of species in the given area of the given group
+     *
+     * @param {String} areaType area type
+     * @param {String | Number} areaId area id
+     * @param {String} group group to filter data (default to all), options are: 'all', 'total',
+     * 'endemic', 'invasive', 'threatened'.
+     *
+     * @returns {Object[]} Number of inferred and observed species for the desired group.
+     */
+  findNumberOfSpecies: async (areaType, areaId) => {
+    try {
+      return db('richness_nos as rn')
+      // .left join richness_nos_regions rnr on rn.id_region = rnr.id_region
+        .select(
+          'rn.total_inf as rn_total_inf',
+          'rn.total_obs as rn_total_obs',
+          'rnr.total_obs as rnr_total_obs',
+          'rnr.total_inf as rnr_total_inf',
+          'rn.end_inf as rn_end_inf',
+          'rn.end_obs as rn_end_obs',
+          'rnr.end_obs as rnr_end_obs',
+          'rnr.end_inf as rnr_end_inf',
+          'rn.inv_inf as rn_inv_inf',
+          'rn.inv_obs as rn_inv_obs',
+          'rnr.inv_obs as rnr_inv_obs',
+          'rnr.inv_inf as rnr_inv_inf',
+          'rn.thr_inf as rn_thr_inf',
+          'rn.thr_obs as rn_thr_obs',
+          'rnr.thr_obs as rnr_thr_obs',
+          'rnr.thr_inf as rnr_thr_inf',
+        )
+        .leftJoin('richness_nos_regions as rnr', 'rn.id_region', 'rnr.id_region')
+        .where({ 'rn.geofence_type': areaTypeKeys(areaType), 'rn.geofence_id': areaId });
+    } catch (e) {
+      logger.error(e.stack || e.Error || e.message || e);
+      throw new Error('Error getting data');
+    }
+  },
+
 });

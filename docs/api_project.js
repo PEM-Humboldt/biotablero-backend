50d53eda1e9393553573714f34a26c30cd21f19e
Trjegul84 <angelachek@gmail.com>
2021-07-22 16:52:34 -0500

    files after run yarn all docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-07-22T20:52:33.803Z",
+    "time": "2021-07-22T21:51:41.127Z",
     "url": "https://apidocjs.com",
     "version": "0.26.0"
   }

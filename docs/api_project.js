18ff7a9e0b6fc92dc40aab4e80af2217cbeb9775
Trjegul84 <angelachek@gmail.com>
2021-07-22 15:49:53 -0500

    add documentacion for all services

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-06-01T19:41:04.619Z",
+    "time": "2021-07-22T20:48:33.066Z",
     "url": "https://apidocjs.com",
     "version": "0.26.0"
   }

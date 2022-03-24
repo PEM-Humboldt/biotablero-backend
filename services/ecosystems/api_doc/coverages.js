6a8521f79c327e980b7258d0efcea29b78c6f79d
Camilo Zapata <cazm013@gmail.com>
2022-03-24 12:29:53 -0500

    Add CoverageSEExample and CoverageSELayerExample

--- services/ecosystems/api_doc/coverages.js
+++ services/ecosystems/api_doc/coverages.js
@@ -30,3 +30,31 @@
  * @apiSuccessExample {binary} Success-Example:
  * image/png
  */
+
+/**
+ * @apiDefine CoverageSEExample
+ * @apiSuccessExample {json} Success-Example:
+ *  [
+ *    {
+ *      "area": 235.44,
+ *      "type": "S",
+ *      "percentage": 0.02115272656704832
+ *    },
+ *    {
+ *      "area": 81.45,
+ *      "type": "N",
+ *      "percentage": 0.007317743709166182
+ *    },
+ *    {
+ *      "area": 10813.59,
+ *      "type": "T",
+ *      "percentage": 0.9715295297237856
+ *    }
+ *  ]
+ */
+
+/**
+ * @apiDefine CoverageSELayerExample
+ * @apiSuccessExample {binary} Success-Example:
+ * image/png
+ */

39b649b164d738d218bee022b34459314519b4f8
Camilo Zapata <cazm013@gmail.com>
2022-03-24 12:37:53 -0500

    Add persistences, services and routes for endpoints related to coverages ans strategic ecosystems

--- services/ecosystems/src/routes/coverage.js
+++ services/ecosystems/src/routes/coverage.js
@@ -25,9 +25,7 @@ module.exports = (errorHandler, coverageService) => {
    *  /coverage?areaType=ea&areaId=DAGMA
    * @apiUse CoverageExample
    */
-  router.get(
-    '/coverage',
-    errorHandler((req, res, next) => {
+  router.get('/coverage', errorHandler((req, res, next) => {
       if (!(req.params.areaType && req.params.areaId)) {
         const error = { code: 400, message: 'areaType and areaId required' };
         throw error;
@@ -46,12 +44,12 @@ module.exports = (errorHandler, coverageService) => {
    * @apiVersion 1.0.0
    * @apiDescription
    * Layer of a specific coverage type within a given area. Parameter type could be:
-   * N (Natural), S(Secundaria), T(Transformada), and X(clouds).
+   * N (Natural), S(Secundaria), T(Transformada), and X(Nubes).
    *
    * @apiParam (Query params) {String} areaType area type
    * @apiParam (Query params) {String|Number} areaId area id
    * @apiParam (Query params) {String} type to select the proper type of coverage. Options are:
-   * N (Natural), S(Secundaria), T(Transformada) and X(Clouds).
+   * N (Natural), S(Secundaria), T(Transformada) and X(Nubes).
    *
    * @apiSuccess {Binary} result image with the geometry
    *
@@ -59,9 +57,7 @@ module.exports = (errorHandler, coverageService) => {
    *  /coverage/layer?areaType=ea&areaId=CARDER&type=T
    * @apiUse CoverageLayerExample
    */
-  router.get(
-    '/coverage/layer',
-    errorHandler((req, res, next) => {
+  router.get('/coverage/layer', errorHandler((req, res, next) => {
       if (!(req.params.areaType && req.params.areaId && req.params.type)) {
         const error = { code: 400, message: 'areaType, areaId and type are required' };
         throw error;
@@ -69,6 +65,124 @@ module.exports = (errorHandler, coverageService) => {
       return coverageService
         .getCoverageLayer(req.params.areaType, req.params.areaId, req.params.type)
         .then((value) => {
+          res.sendRaw(200, value, { 'Content-Type': 'image/png' });
+          next();
+        });
+    }),
+  );
+
+  /**
+   * @apiGroup s_strategic_ecosystem
+   * @api {get} /se SEAreas
+   * @apiName SEAreas
+   * @apiVersion 1.0.0
+   * @apiDescription
+   * Area distribution for each SE type and total SE area within a given area
+   *
+   * Values calculated for 2018
+   *
+   * @apiParam (Query params) {String} areaType area type
+   * @apiParam (Query params) {String|Number} areaId area id
+   *
+   * @apiSuccess {Object[]} result
+   * @apiSuccess {Number} result.area area of the each strategic ecosystem type and total
+   * @apiSuccess {String} result.type strategic ecosystem type
+   *
+   * @apiExample {curl} Example usage:
+   *  /se?areaType=ea&areaId=DAGMA
+   * @apiUse SEAreasExample
+   */
+  router.get('/se', errorHandler((req, res, next) => {
+      if (!(req.params.areaType && req.params.areaId)) {
+        const error = { code: 400, message: 'areaType and areaId required'};
+        throw error;
+      }
+      return coverageService.getSEAreas(req.params.areaType,req.params.areaId).then((value) => {
+        res.send(value);
+        next();
+      });
+    }),
+  );
+
+  /**
+   * @apiGroup s_coverage
+   * @api {get} /coverage/se CoverageSE
+   * @apiName CoverageSE
+   * @apiVersion 1.0.0
+   * @apiDescription
+   * Area distribution for each coverage type within a SE type and a given area
+   *
+   * Values calculated for 2018
+   *
+   * @apiParam (Query params) {String} areaType area type
+   * @apiParam (Query params) {String|Number} areaId area id
+   * @apiParam (Query params) {String} seType strategic ecosystem type
+   *
+   * @apiSuccess {Object[]} result
+   * @apiSuccess {Number} result.area area of the specified coverage type
+   * @apiSuccess {String} result.type coverage type
+   * @apiSuccess {Number} result.percentage percentage of the specified coverage type
+   *
+   * @apiExample {curl} Example usage:
+   *  /coverage/se?areaType=ea&areaId=DAGMA&seType=Páramo
+   * @apiUse CoverageSEExample
+   */
+   router.get('/coverage/se',errorHandler((req, res, next) => {
+      if (!(req.params.areaType && req.params.areaId && req.params.seType)) {
+        const error = { code: 400, message: 'areaType, areaId and seType required'};
+        throw error;
+      }
+      return coverageService.getCoverageSE(
+        req.params.areaType,
+        req.params.areaId,
+        req.params.seType
+      ).then((value) => {
+        res.send(value);
+        next();
+      });
+    }),
+  );
+
+  /**
+   * @apiGroup s_coverage
+   * @api {get} /coverage/se/layer CoverageSELayer
+   * @apiName CoverageSELayer
+   * @apiVersion 1.0.0
+   * @apiDescription
+   * Layer of a specific coverage type and se type within a given area. Options for coverageType:
+   * N (Natural), S(Secundaria), T(Transformada), and X(Nubes). Options for seType: Páramo, 
+   * Bosque Seco Tropical and Humedal.
+   *
+   * @apiParam (Query params) {String} areaType area type
+   * @apiParam (Query params) {String|Number} areaId area id
+   * @apiParam (Query params) {String} coverageType to select the proper type of coverage. 
+   * Options are: N (Natural), S(Secundaria), T(Transformada) and X(Nubes).
+   * @apiParam (Query params) {String} seType to select the proper type of strategic ecosystem. 
+   * Options are: Páramo, Bosque Seco Tropical and Humedal.
+   *
+   * @apiSuccess {Binary} result image with the geometry
+   *
+   * @apiExample {curl} Example usage:
+   *  /coverage/se/layer?areaType=ea&areaId=CARDER&coverageType=N&seType=Páramo
+   * @apiUse CoverageSELayerExample
+   */
+   router.get('/coverage/se/layer', errorHandler((req, res, next) => {
+      if (!(
+        req.params.areaType && 
+        req.params.areaId && 
+        req.params.coverageType && 
+        req.params.seType
+      )) {
+        const error = { code: 400, message: 'areaType, areaId and type are required' };
+        throw error;
+      }
+      return coverageService
+        .getCoverageSELayer(
+          req.params.areaType,
+          req.params.areaId,
+          req.params.coverageType,
+          req.params.seType
+        ).then((value) => {
           res.sendRaw(200, value, { 'Content-Type': 'image/png' });
           next();
         });
--- services/ecosystems/src/services/coverage.js
+++ services/ecosystems/src/services/coverage.js
@@ -1,4 +1,8 @@
-const { rasterCoverageKeys, areaTypeKeys } = require('../util/appropriate_keys');
+const { 
+  areaTypeKeys,
+  rasterCoverageKeys,
+  rasterCoverageSEKeys,
+} = require('../util/appropriate_keys');
 const { coveragesColorSet } = require('../util/colorSet');
 
 module.exports = (CoveragePersistence, restAPI) => {
@@ -54,6 +58,138 @@ module.exports = (CoveragePersistence, restAPI) => {
         throw error;
       }
     },
+
+    /**
+     * Get area distribution for each SE type and total SE area within a given area
+     *
+     * @param {String} areaType area type
+     * @param {String | Number} areaId area id
+     *
+     * @returns {Object[]} Values of area distribution for each SE type and total SE area
+     */
+
+    getSEAreas: async (areaType, areaId) => {
+      const data = [];
+
+      const rawDataP = await CoveragePersistence.findCoverageSEParamo(
+        areaTypeKeys(areaType),
+        areaId
+      );
+      data.push({
+        area: rawDataP.map((i) => i.area_ha).reduce((prev, next) => prev + next),
+        type: 'Páramo',
+      });
+
+      const rawDataDF = await CoveragePersistence.findCoverageSEDryForest(
+        areaTypeKeys(areaType),
+        areaId
+      );
+      data.push({
+        area: rawDataDF.map((i) => i.area_ha).reduce((prev, next) => prev + next),
+        type: 'Bosque Seco Tropical',
+      });
+
+      const rawDataW = await CoveragePersistence.findCoverageSEWetland(
+        areaTypeKeys(areaType),
+        areaId
+      );
+      data.push({
+        area: rawDataW.map((i) => i.area_ha).reduce((prev, next) => prev + next),
+        type: 'Humedal',
+      });
+
+      const noSEData = data.every((item) => Object.values(item).length === 0);
+      if (noSEData) {
+        throw new Error("Data for SE Areas doesn't exists in the selected area id and area type");
+      }
+
+      data.unshift({
+        area: data.map((i) => i.area).reduce((prev, next) => prev + next),
+        type: 'Total',
+      });
+
+      return data;
+    },
+
+    /**
+     * Get area distribution for each coverage type within a SE type and a given area
+     *
+     * @param {String} areaType area type
+     * @param {String | Number} areaId area id
+     * @param {String} seType strategic ecosystem type, options are: 'Páramo','Bosque Seco Tropical'
+     *  and 'Humedal'
+     *
+     * @returns {Object[]} Values of area distribution by coverage type
+     */
+    getCoverageSE: async (areaType, areaId, seType) => {
+      let rawData = null;
+      switch (seType) {
+        case 'Bosque Seco Tropical':
+          rawData = await CoveragePersistence.findCoverageSEDryForest(
+            areaTypeKeys(areaType),
+            areaId
+          );
+        break;
+        case 'Páramo':
+          rawData = await CoveragePersistence.findCoverageSEParamo(
+            areaTypeKeys(areaType),
+            areaId
+          );
+        break;
+        case 'Humedal':
+          rawData = await CoveragePersistence.findCoverageSEWetland(
+            areaTypeKeys(areaType),
+            areaId
+          );
+        break;
+        default:
+          throw new Error("Data doesn't exist for the given se type");
+      }
+
+      const noCoverageData = rawData.every((item) => Object.values(item).length === 0);
+      if (noCoverageData) {
+        throw new Error("Data for coverage doesn't exists in the selected area id, area type and se type");
+      }
+
+      const totalArea = rawData.map((i) => i.area_ha).reduce((prev, next) => prev + next);
+      const data = Object.keys(rawData).map((key) => ({
+        area: rawData[key].area_ha,
+        type: rawData[key].area_type,
+        percentage: Number(rawData[key].area_ha) / totalArea,
+      }));
+      const invalidValues = data.filter((obj) => !obj.area || obj.area === 0);
+      return invalidValues.length === data.length ? [] : data;
+    },
+  
+    /**
+     * Get the coverage layer according to its type in a given area
+     *
+     * @param {String} areaType area type
+     * @param {String | Number} areaId area id
+     * @param {String} coverageType coverage type, options are: 'N', 'S', 'T' and 'X'
+     * @param {String} seType strategic ecosystem type, options are: 'Páramo','Bosque Seco Tropical'
+     *  and 'Humedal'
+     *
+     * @returns {Binary} Image with the geometry
+     */
+    getCoverageSELayer: async (areaType, areaId, coverageType, seType) => {
+      try {
+        const areaGeom = await restAPI.requestAreaGeometry(areaType, areaId);
+        return CoveragePersistence.findCoverageLayer(
+          areaGeom.features[0].geometry,
+          rasterCoverageSEKeys(seType, coverageType),
+          coveragesColorSet(coverageType),
+        );
+      } catch (e) {
+        const error = {
+          code: 500,
+          stack: e.stack,
+          message: 'Error retrieving layer',
+        };
+        throw error;
+      }
+    },
   };
+
   return Coverage;
 };

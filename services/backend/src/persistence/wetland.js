617cbf53ced5ae1d90b87a6d1e35d4d5c07d1019
Camilo Zapata <cazm013@gmail.com>
2022-04-07 14:55:30 -0500

    Delete persistences

--- services/backend/src/persistence/wetland.js
+++ services/backend/src/persistence/wetland.js
@@ -1,39 +1,4 @@
 module.exports = (db, { colombiaWetlandDetails, geoHFWetland }) => ({
-  /**
-   * Get the area inside the given environmental authority
-   *
-   * @param {String} eaId environmental authority id
-   * @param {Number} year optional year to filter data, 2012 by default
-   */
-  findAreaByEA: (eaId, year = 2012) =>
-    colombiaWetlandDetails
-      .query()
-      .where({ id_ea: eaId, year_cover: year })
-      .select(db.raw('coalesce(SUM(area_ha), 0) as area')),
-
-  /**
-   * Get the area inside the given basin subzone
-   *
-   * @param {String} subzoneId basin subzone id
-   * @param {Number} year optional year to filter data, 2012 by default
-   */
-  findAreaBySubzone: (subzoneId, year = 2012) =>
-    colombiaWetlandDetails
-      .query()
-      .where({ id_subzone: subzoneId, year_cover: year })
-      .select(db.raw('coalesce(SUM(area_ha), 0) as area')),
-
-  /**
-   * Get the area inside the given state
-   *
-   * @param {String} stateId state id
-   * @param {Number} year optional year to filter data, 2012 by default
-   */
-  findAreaByState: (stateId, year = 2012) =>
-    colombiaWetlandDetails
-      .query()
-      .where({ id_state: stateId, year_cover: year })
-      .select(db.raw('coalesce(SUM(area_ha), 0) as area')),
 
   /**
    * Find total area
@@ -58,36 +23,6 @@ module.exports = (db, { colombiaWetlandDetails, geoHFWetland }) => ({
       .orderBy('type'),
 
   /**
-   * Find areas grouped by cover type inside the given environmental authority
-   *
-   * @param {String} eaId environmental authority id
-   * @param {Number} year optional year to filter data, 2012 by default
-   */
-  findCoverAreasInEA: async (eaId, year = 2012) =>
-    colombiaWetlandDetails
-      .query()
-      .where({ id_ea: eaId, year_cover: year })
-      .sum('area_ha as area')
-      .groupBy('area_type')
-      .select('area_type as type')
-      .orderBy('type'),
-
-  /**
-   * Find areas grouped by cover type inside the given state
-   *
-   * @param {String} stateId state id
-   * @param {Number} year optional year to filter data, 2012 by default
-   */
-  findCoverAreasInState: async (stateId, year = 2012) =>
-    colombiaWetlandDetails
-      .query()
-      .where({ id_state: stateId, year_cover: year })
-      .sum('area_ha as area')
-      .groupBy('area_type')
-      .select('area_type as type')
-      .orderBy('type'),
-
-  /**
    * Find areas grouped by protected area category inside the given state
    *
    * @param {String} stateId state id
@@ -104,21 +39,6 @@ module.exports = (db, { colombiaWetlandDetails, geoHFWetland }) => ({
       .groupBy('gbpa.label', 'gbpa.binary_protected')
       .select(db.raw('coalesce(SUM(cwd.area_ha), 0) as area'), 'gbpa.label as type')
       .orderBy('gbpa.binary_protected', 'desc'),
-
-  /**
-   * Find areas grouped by cover type inside the given basin subzone
-   *
-   * @param {String} subzoneId basin subzone id
-   * @param {Number} year optional year to filter data, 2012 by default
-   */
-  findCoverAreasInSubzone: async (subzoneId, year = 2012) =>
-    colombiaWetlandDetails
-      .query()
-      .where({ id_subzone: subzoneId, year_cover: year })
-      .sum('area_ha as area')
-      .groupBy('area_type')
-      .select('area_type as type')
-      .orderBy('type'),
 
   /**
    * Find areas grouped by protected area category inside the given subzone

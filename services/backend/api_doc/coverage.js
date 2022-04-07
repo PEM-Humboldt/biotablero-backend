68cca46aa23911ac25a0ac96313972ccb2f01dde
Camilo Zapata <cazm013@gmail.com>
2022-04-07 14:53:36 -0500

    Delete api doc example

--- services/backend/api_doc/coverage.js
+++ services/backend/api_doc/coverage.js
@@ -20,20 +20,3 @@
  *    }
  *  ]
  */
-
-/**
- * @apiDefine SECoverageInGeofenceExample
- * @apiSuccessExample {json} Success-Example:
- *  [
- *    {
- *      "percentage": 0.4437728527,
- *      "area": "1493.945506792712753",
- *      "type": "N"
- *    },
- *    {
- *      "percentage": 0.5562271473,
- *      "area": "158.998859058673413",
- *      "type": "T"
- *    }
- *  ]
- */

02f9365fe5e17523a70c298040e14641060624c0
Camilo Zapata <cazm013@gmail.com>
2022-04-19 12:17:57 -0500

    Restore persistences to get the area inside dry forest and wetland

--- services/backend/src/persistence/wetland.js
+++ services/backend/src/persistence/wetland.js
@@ -1,5 +1,41 @@
 module.exports = (db, { colombiaWetlandDetails, geoHFWetland }) => ({
   /**
+   * Get the area inside the given environmental authority
+   *
+   * @param {String} eaId environmental authority id
+   * @param {Number} year optional year to filter data, 2012 by default
+   */
+  findAreaByEA: (eaId, year = 2012) =>
+    colombiaWetlandDetails
+      .query()
+      .where({ id_ea: eaId, year_cover: year })
+      .select(db.raw('coalesce(SUM(area_ha), 0) as area')),
+
+  /**
+   * Get the area inside the given basin subzone
+   *
+   * @param {String} subzoneId basin subzone id
+   * @param {Number} year optional year to filter data, 2012 by default
+   */
+  findAreaBySubzone: (subzoneId, year = 2012) =>
+    colombiaWetlandDetails
+      .query()
+      .where({ id_subzone: subzoneId, year_cover: year })
+      .select(db.raw('coalesce(SUM(area_ha), 0) as area')),
+
+  /**
+   * Get the area inside the given state
+   *
+   * @param {String} stateId state id
+   * @param {Number} year optional year to filter data, 2012 by default
+   */
+  findAreaByState: (stateId, year = 2012) =>
+    colombiaWetlandDetails
+      .query()
+      .where({ id_state: stateId, year_cover: year })
+      .select(db.raw('coalesce(SUM(area_ha), 0) as area')),
+
+  /**
    * Find total area
    *
    * @param {Number} year optional year to filter data, 2012 by default

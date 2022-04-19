a1cd869f575133cd82dafd783c33fa55d3f7c4ef
Erika Suárez Valencia <1734998+erikasv@users.noreply.github.com>
2022-04-19 10:25:53 -0500

    Restore some se functions

--- services/backend/src/persistence/wetland.js
+++ services/backend/src/persistence/wetland.js
@@ -12,6 +12,30 @@ module.exports = (db, { colombiaWetlandDetails, geoHFWetland }) => ({
       .select(db.raw('coalesce(SUM(area_ha), 0) as area')),
 
   /**
+   * Get the area inside the given basin subzone
+   *
+   * @param {String} subzoneId basin subzone id
+   * @param {Number} year optional year to filter data, 2012 by default
+   */
+  findAreaBySubzone: (subzoneId, year = 2012) =>
+    colombiaWetlandDetails
+      .query()
+      .where({ id_subzone: subzoneId, year_cover: year })
+      .select(db.raw('coalesce(SUM(area_ha), 0) as area')),
+
+  /**
+   * Get the area inside the given state
+   *
+   * @param {String} stateId state id
+   * @param {Number} year optional year to filter data, 2012 by default
+   */
+  findAreaByState: (stateId, year = 2012) =>
+    colombiaWetlandDetails
+      .query()
+      .where({ id_state: stateId, year_cover: year })
+      .select(db.raw('coalesce(SUM(area_ha), 0) as area')),
+
+  /**
    * Find total area
    *
    * @param {Number} year optional year to filter data, 2012 by default

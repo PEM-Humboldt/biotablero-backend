76d4be9f2ad9d0903814bc4d5b772a7347ea3951
Camilo Zapata <cazm013@gmail.com>
2022-06-24 10:16:19 -0500

    Persistence, services and route to texts endpoint

---
+++ services/util/src/persistence/texts.js
@@ -0,0 +1,21 @@
+module.exports = (db, { texts }, logger) => {
+  const TextsPersistence = {
+    /**
+     * Find texts that describe one section in the frontend
+     *
+     * @param {String} key text key
+     *
+     * @returns {Object} Texts of info, methodology, considerations and quote
+     */
+    findTexts: (key) =>
+      texts
+        .query()
+        .select('type', 'text')
+        .where({ key })
+        .catch((e) => {
+          logger.error(e.stack || e.Error || e.message || e);
+          throw new Error('Error getting data');
+        }),
+  };
+  return TextsPersistence;
+};
---
+++ services/util/src/services/util.js
@@ -0,0 +1,32 @@
+module.exports = (
+  TextsPersistence,
+) => {
+  const Util = {
+    /**
+     * Texts that describe one section in the frontend
+     *
+     * @param {String} key text key
+     *
+     * @returns {Object} Object with texts of info, methodology, considerations and quote
+     */
+    getTexts: async (key) => {
+      const rawData = await TextsPersistence.findTexts(key);
+      let data = {};
+
+      if(!rawData || rawData.length === 0) {
+        throw new Error("There is not data of texts asociated to the given key");
+      }
+
+      rawData.forEach(element => {
+        data = {
+          ...data,
+          [element.type]: element.text,
+        };
+      });
+
+      return data;
+    },
+  };
+
+  return Util;
+};

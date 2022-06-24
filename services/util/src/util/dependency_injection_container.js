9731a94d06a3d4a7432f5b00ca0ad64c4dd5ce9c
Camilo Zapata <cazm013@gmail.com>
2022-06-24 10:15:43 -0500

    Add persistence, service and route to new endpoint that returns texts of ine section

--- services/util/src/util/dependency_injection_container.js
+++ services/util/src/util/dependency_injection_container.js
@@ -3,14 +3,29 @@ const Bottlejs = require('bottlejs');
 const ErrorHandler = require('./errorHandler');
 const logger = require('./logger');
 
-// eslint-disable-next-line no-unused-vars
 const bookshelfModels = require('../persistence/models/setup');
+
+const TextsPersistence = require('../persistence/texts');
+
+const UtilService = require('../services/util');
+
+const UtilRoute = require('../routes/util');
 
 const bottle = new Bottlejs();
 
 bottle.factory('logger', () => logger);
 bottle.factory('errorHandler', (container) => ErrorHandler(container.logger));
 
-bottle.factory('routes', () => []);
+bottle.factory('TextsPersistence', () =>
+  TextsPersistence(bookshelfModels.db, bookshelfModels.models, logger),
+);
+
+bottle.factory('UtilService', (container) =>
+  UtilService(container.TextsPersistence),
+);
+
+bottle.factory('routes', (container) => [
+  UtilRoute(container.errorHandler, container.UtilService),
+]);
 
 module.exports = bottle.container;

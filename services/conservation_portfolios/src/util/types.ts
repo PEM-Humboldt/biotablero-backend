3c2983eba6a18106d4c8bbe7f59e06dfdb4ee1cc
Erika Suárez Valencia <1734998+erikasv@users.noreply.github.com>
2022-09-13 08:56:10 -0500

    Fix RouterFunction type to allow it to return a promise

--- services/conservation_portfolios/src/util/types.ts
+++ services/conservation_portfolios/src/util/types.ts
@@ -3,6 +3,6 @@ import winston from 'winston';
 
 export type Logger = winston.Logger;
 
-export type RouterFunction = (req: Request, res: Response, next: Next) => void;
+export type RouterFunction = (req: Request, res: Response, next: Next) => void | Promise<void>;
 
 export type EHFunction = (callback: RouterFunction) => RouterFunction;

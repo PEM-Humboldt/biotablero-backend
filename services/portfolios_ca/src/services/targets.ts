964c765c1f02da6159e77be16f3b2ee9d3e9227d
Manuel Gálvez <mgalvez@humboldt.org.co>
2022-10-06 20:56:19 -0500

    Add element to portfolios mockup data

--- services/portfolios_ca/src/services/targets.ts
+++ services/portfolios_ca/src/services/targets.ts
@@ -9,45 +9,91 @@ export default () => {
      *
      * @returns {Object} Target basic information and values of portfolios
      */
-    getPortfoliosByTarget: async () => ({
-      target_id: 3,
-      target_name: 'Servicios Ecosistémicos',
-      target_national: 4521,
-      target_units_short: 'TC',
-      target_units: 'Toneladas de carbono',
-      portfolios_data: [
+    getPortfoliosByTarget: async (targetId: number) => {
+      const data = [
         {
-          id: 1,
-          name: 'NATURE MAP · WCMC',
-          short_name: 'WCMC',
-          value: 100,
+          target_id: 2,
+          target_name: 'Ecosistemas',
+          target_national: 4521,
+          target_units_short: 'TC',
+          target_units: 'Toneladas de carbono',
+          portfolios_data: [
+            {
+              id: 1,
+              name: 'NATURE MAP · WCMC',
+              short_name: 'WCMC',
+              value: 100,
+            },
+            {
+              id: 2,
+              name: 'ELSA · PNUD',
+              short_name: 'ELSA',
+              value: 200,
+            },
+            {
+              id: 3,
+              name: 'WEPLAN FORESTS',
+              short_name: 'WEPLAN',
+              value: 300,
+            },
+            {
+              id: 4,
+              name: 'Especies, Carbono y Agua . LONG NAME',
+              short_name: 'Especies, Carbono y Agua',
+              value: 400,
+            },
+            {
+              id: 5,
+              name: 'ACC · LONG NAME',
+              short_name: 'ACC',
+              value: 500,
+            },
+          ],
         },
         {
-          id: 2,
-          name: 'ELSA · PNUD',
-          short_name: 'ELSA',
-          value: 200,
+          target_id: 3,
+          target_name: 'Servicios Ecosistémicos',
+          target_national: 4521,
+          target_units_short: 'TC',
+          target_units: 'Toneladas de carbono',
+          portfolios_data: [
+            {
+              id: 1,
+              name: 'NATURE MAP · WCMC',
+              short_name: 'WCMC',
+              value: 420,
+            },
+            {
+              id: 2,
+              name: 'ELSA · PNUD',
+              short_name: 'ELSA',
+              value: 250,
+            },
+            {
+              id: 3,
+              name: 'WEPLAN FORESTS',
+              short_name: 'WEPLAN',
+              value: 330,
+            },
+            {
+              id: 4,
+              name: 'Especies, Carbono y Agua . LONG NAME',
+              short_name: 'Especies, Carbono y Agua',
+              value: 0,
+            },
+            {
+              id: 5,
+              name: 'ACC · LONG NAME',
+              short_name: 'ACC',
+              value: 80,
+            },
+          ],
         },
-        {
-          id: 3,
-          name: 'WEPLAN FORESTS',
-          short_name: 'WEPLAN',
-          value: 300,
-        },
-        {
-          id: 4,
-          name: 'Especies, Carbono y Agua . LONG NAME',
-          short_name: 'Especies, Carbono y Agua',
-          value: 400,
-        },
-        {
-          id: 5,
-          name: 'ACC · LONG NAME',
-          short_name: 'ACC',
-          value: 500,
-        },
-      ],
-    }),
+      ];
+      const elementId = targetId % 2 == 0 ? 0 : 1;
+
+      return data[elementId];
+    },
 
     /**
      * Get list of targets with portfolios values within a given area
@@ -57,7 +103,7 @@ export default () => {
      *
      * @returns {Object[]} List of targets with portfolios values
      */
-    getTargetsList: async () => [
+    getTargetsList: async (): Promise<object[]> => [
       {
         id: 1,
         name: 'Especies',

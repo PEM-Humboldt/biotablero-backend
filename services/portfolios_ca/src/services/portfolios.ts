b5ee04a667950a4850dc8f04f6ceffec3d9e3526
Manuel Gálvez <mgalvez@humboldt.org.co>
2022-10-28 11:03:36 -0500

    Fix else after return in conditional

--- services/portfolios_ca/src/services/portfolios.ts
+++ services/portfolios_ca/src/services/portfolios.ts
@@ -56,13 +56,13 @@ export default () => {
       }
       if (portfolioId === 4) {
         return readFile(`${folder}/portafolios-ca_layer_5_3.png`);
-      } else {
-        const error = {
-          code: 404,
-          message: 'No layers for this portfolio',
-        };
-        throw error;
       }
+
+      const error = {
+        code: 404,
+        message: 'No layers for this portfolio',
+      };
+      throw error;
     },
   };
   return Portfolios;
